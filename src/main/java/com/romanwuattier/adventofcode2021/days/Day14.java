2b08ba25405cd6c9198beb140791a1a7ab557e31
RomanWuattier <roman.wuattier@gmail.com>
2021-12-16 20:45:25 +0100

    Day 14: Extended Polymerization

--- src/main/java/com/romanwuattier/adventofcode2021/days/Day14.java
+++ src/main/java/com/romanwuattier/adventofcode2021/days/Day14.java
@@ -1,13 +1,15 @@
 package com.romanwuattier.adventofcode2021.days;
 
-import static com.romanwuattier.adventofcode2021.helper.Helpers.sliding;
-
 import com.romanwuattier.adventofcode2021.common.Day;
-import java.util.Comparator;
+
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static com.romanwuattier.adventofcode2021.helper.Helpers.sliding;
+
+/** Day 14: Extended Polymerization */
 public class Day14 implements Day {
   public static void main(String[] args) {
     new Day14().printParts();
@@ -22,31 +24,47 @@ public class Day14 implements Day {
 
   @Override
   public Object part1() {
-    var res = template;
-    for (int step = 0; step < 10; step++) {
-      res = run(res);
-    }
-    var count = new HashMap<Character, Long>();
-    res.chars().mapToObj(i -> (char) i).forEach(c -> count.put(c, count.getOrDefault(c, 0L) + 1));
-    var min = count.values().stream().min(Comparator.comparingLong(a -> a)).orElseThrow();
-    var max = count.values().stream().max(Comparator.comparingLong(a -> a)).orElseThrow();
-    return max - min;
+    return run(10);
   }
 
   @Override
   public Object part2() {
-    return null;
+    return run(40);
   }
 
-  private String run(String tmpl) {
-    var sb = new StringBuilder();
-    sliding(tmpl.chars().mapToObj(i -> (char) i).map(c -> Character.toString(c)).toList(), 2)
+  private long run(int steps) {
+    var polymers = rules.keySet().stream().collect(Collectors.toMap(polymer -> polymer, __ -> 0L));
+    var acc = rules.keySet().stream().collect(Collectors.toMap(polymer -> polymer, __ -> 0L));
+    var counts = new HashMap<Character, Long>();
+
+    sliding(template.chars().mapToObj(i -> (char) i).map(c -> Character.toString(c)).toList(), 2)
         .forEach(
             pair -> {
-              var concat = pair.get(0) + pair.get(1);
-              var toInsert = rules.get(concat);
-              sb.append(pair.get(0)).append(toInsert);
+              var polymer = pair.get(0) + pair.get(1);
+              polymers.put(polymer, polymers.get(polymer) + 1);
             });
-    return sb.append(template.charAt(template.length() - 1)).toString();
+
+    for (var step = 0; step < steps; step++) {
+      for (var polymer : polymers.keySet()) {
+        if (polymers.get(polymer) > 0) {
+          var fromLeft = polymer.charAt(0) + rules.get(polymer);
+          var fromRight = rules.get(polymer) + polymer.charAt(1);
+          acc.put(fromLeft, acc.get(fromLeft) + polymers.get(polymer));
+          acc.put(fromRight, acc.get(fromRight) + polymers.get(polymer));
+        }
+      }
+
+      for (var polymer : acc.keySet()) {
+        polymers.put(polymer, acc.get(polymer));
+        acc.put(polymer, 0L);
+      }
+    }
+
+    counts.put(template.charAt(template.length() - 1), 1L);
+    for (var polymer : polymers.keySet()) {
+      counts.putIfAbsent(polymer.charAt(0), 0L);
+      counts.put(polymer.charAt(0), counts.get(polymer.charAt(0)) + polymers.get(polymer));
+    }
+    return Collections.max(counts.values()) - Collections.min(counts.values());
   }
 }
---
+++ src/test/java/com/romanwuattier/adventofcode2021/days/Day14Test.java
@@ -0,0 +1,18 @@
+package com.romanwuattier.adventofcode2021.days;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day14Test {
+
+  @Test
+  void part1() {
+    assertEquals(3058L, new Day14().part1());
+  }
+
+  @Test
+  void part2() {
+    assertEquals(3447389044530L, new Day14().part2());
+  }
+}
